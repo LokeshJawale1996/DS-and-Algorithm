bbbd3ff2afb08a30fb23380836ee2f65643fce86
LokeshJawale1996 <jawalelokesh2411@gmail.com>
2022-07-04 14:06:51 +0530

    challenge 1 sum zero number

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # DS-and-Algorithm-
+
 some JavaScript coding challenges related to DS AND Algorithm
+
+-[Sum Zero Number o^2](./challenges/problem1.js)
---
+++ challenges/problem1.js
@@ -0,0 +1,17 @@
+//checking sum zero -Problem 1
+//[-5,-4,-3,-2,0,2,4,6,8]
+//[-4,4] ->output
+
+function getSumPairZero(array) {
+  for (let number of array) {
+    for (let j = 1; j < array.length; j++) {
+      if (number + array[j] === 0) {
+        return [number, array[j]];
+      }
+    }
+  }
+}
+//o(n^2) quadratic time complexity
+
+const result = getSumPairZero([-5, -4, -3, -2, 0, 2, 4, 6, 8]);
+console.log(result);
